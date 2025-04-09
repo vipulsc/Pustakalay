86d3031bf1728a70d8dd4a554ee7ae01ccdaea7d
Vipul Singh <vipulsingh.1404@gmail.com>
2025-04-09 12:10:00 +0530

    models-completed

---
+++ db/models/order.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const order = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "user",
+    },
+    book: {
+      type: mongoose.Types.ObjectId,
+      ref: "books",
+    },
+    status: {
+      type: String,
+      default: "Order Placed",
+      enum: ["Order Placed", "Out for delivery", "Delivered", "Cancelled"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+model.exports = mongoose.model("order", order);
